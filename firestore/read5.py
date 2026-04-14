9333e922b653095b27250295ea75454204cd338f
Andy55-coder <jimmy0937505327@gmail.com>
2026-04-14 15:40:17 +0800

    Add files via upload

--- firestore/read5.py
+++ firestore/read5.py
@@ -2,30 +2,17 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 from google.cloud.firestore_v1.base_query import FieldFilter
 
-# 初始化 Firebase（避免重複）
-if not firebase_admin._apps:
-    cred = credentials.Certificate("serviceAccountKey.json")
-    firebase_admin.initialize_app(cred)
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
 collection_ref = db.collection("靜宜資管")
+#docs = collection_ref.where(filter=FieldFilter("mail","==", "tcyang@pu.edu.tw")).get()
+docs = collection_ref.get()
 
-# 使用者輸入
-keyword = input("請輸入老師名字：")
-
-# 🔥 Firestore 查詢
-docs = collection_ref\
-    .where(filter=FieldFilter("name", ">=", keyword))\
-    .where(filter=FieldFilter("name", "<=", keyword + "\uf8ff"))\
-    .get()
-
-found = False
-
+keywoed = input("您要查詢老師的名字")
 for doc in docs:
     user = doc.to_dict()
-    print(f"{user['name']}老師的研究室在 {user['lab']}")
-    found = True
-
-if not found:
-    print("查無資料")
+    if keyword in user ["name"]
+        print(f"{user['name']}老師的研究室在{user['lab']}")
