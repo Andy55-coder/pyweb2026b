f859bce41572cd22096f987790ac67ecc36211c4
Andy55-coder <jimmy0937505327@gmail.com>
2026-04-21 11:56:27 +0800

    Add files via upload

---
+++ spider/spider1.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://pyweb2026b-haz9.vercel.app/about"
+Data = requests.get(url)
+Data.encoding = "utf-8"
+#print(Data.text)
+sp = BeautifulSoup(Data.text, "html.parser")
+result=sp.select("a")
+
+for item in result:
+	print(item)
+	print()
+
---
+++ spider/spider3.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://pyweb2026b-haz9.vercel.app/about"
+Data = requests.get(url)
+Data.encoding = "utf-8"
+#print(Data.text)
+sp = BeautifulSoup(Data.text, "html.parser")
+result=sp.find("a")
+
+for item in result:
+	print(item)
+	print(item.get("src"))
+	print()
+
